29c6c09afbc0cab8a19170d761860603bf6acc68
radhakrishna <radhakrishnabhk@gmail.com>
2015-09-30 17:57:38 -0400

    Adds authentication functionality

--- config/app.php
+++ config/app.php
@@ -145,6 +145,7 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 		Riari\Forum\ForumServiceProvider::class,
+		Bernardino\EasyAuthenticator\EasyAuthenticatorServiceProvider::class,
 
     ],
 
